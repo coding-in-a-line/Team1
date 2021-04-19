092f8bd03f7f6c69c11ddbea2befe31ba4d93c13
SangOk <sangok1993@naver.com>
2021-04-19 13:34:07 +0900

    update

---
+++ backend_mariaDB/src/main/java/com/spring/backend_mariadb/BackendMariaDbApplication.java
@@ -0,0 +1,13 @@
+package com.spring.backend_mariadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMariaDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendMariaDbApplication.class, args);
+    }
+
+}
---
+++ backend_mariaDB/src/test/java/com/spring/backend_mariadb/BackendMariaDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.backend_mariadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMariaDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
