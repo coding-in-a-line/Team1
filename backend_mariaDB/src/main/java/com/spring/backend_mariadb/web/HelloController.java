565c568302f0502480a956e04f49fb315eaf0d75
SangOk <sangok1993@naver.com>
2021-04-19 17:04:23 +0900

    log추가

--- backend_mariaDB/src/main/java/com/spring/backend_mariadb/web/HelloController.java
+++ backend_mariaDB/src/main/java/com/spring/backend_mariadb/web/HelloController.java
@@ -1,14 +1,17 @@
 package com.spring.backend_mariadb.web;
 
 import com.spring.backend_mariadb.web.dto.HelloResponseDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
     @GetMapping("/hello")
     public String hello(){
+        log.info("hello");
         return "hello";
     }
 
